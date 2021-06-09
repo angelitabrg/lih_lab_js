79b0cef7b6f3ccb2606fe56d9e579a93c21a7e19
Angelita Regis Guedes <71788259+angelitabrg@users.noreply.github.com>
2021-06-09 16:20:40 -0300

    Added Hello Girl and some operations and conditionals

---
+++ 01_arithmetic_operation/01_arithmetic_operation.js
@@ -0,0 +1,45 @@
+//==sum==
+
+var n1 = 1;
+var n2 = 2;
+
+var sum = n1 + n2;
+
+console.log("The result of the sum is:", sum);
+
+//==multiplication==
+
+var n3 = 5;
+var n4 = 6;
+
+var mult = n3 * n4;
+
+console.log("The result of the multiplication is:", mult);
+
+//==division==
+
+var n5 = 7;
+var n6 = 8;
+
+var division = n5 / n6;
+
+console.log("The result of the division is:", division);
+
+//==exponential==
+
+var n7 = 5;
+var n8 = 2;
+
+var exponential = n7 ** n8;
+
+console.log("The result of the exponential is:", exponential);
+
+//==rest of division==
+
+var n9 = 6;
+var n10 = 2;
+
+var rest = n9 % n10;
+
+console.log("The result of the rest of division is:", rest);
+
---
+++ 02_conditional/01_age.js
@@ -0,0 +1,20 @@
+var age = 21;
+
+if(age>=60){
+    console.log("You are a lady!");
+
+}else if(age>=25){
+    console.log("You are an adult!");
+
+}else if(age>=18){
+    console.log("You are a young!");
+
+}else if(age>=12){
+    console.log("You are a teenager!");
+
+}else if(age>=2){
+    console.log("You are a child!");
+
+}else{
+    console.log("You are a baby!");
+}
